8b38f49c0a69703bb0a2b3bb9623ec7631b64f3d
Wu Liangqing <wlq@rock-chips.com>
2020-12-07 22:10:45 +0800

    rk3566_eink: set PRODUCT_UBOOT_CONFIG := rk3566
    Change-Id: Ibfc4f3e5d92581eb68475686360800f91c87d736

--- rk3566_eink/BoardConfig.mk
+++ rk3566_eink/BoardConfig.mk
@@ -23,7 +23,7 @@ ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
     include device/rockchip/common/BoardConfig_AB.mk
     TARGET_RECOVERY_FSTAB := device/rockchip/rk356x/rk3566_r/recovery.fstab_AB
 endif
-PRODUCT_UBOOT_CONFIG := rk3568
+PRODUCT_UBOOT_CONFIG := rk3566
 PRODUCT_KERNEL_DTS := rk3566-evb2-lp4x-v10-eink
 BOARD_GSENSOR_MXC6655XA_SUPPORT := true
 BOARD_CAMERA_SUPPORT_EXT := true
