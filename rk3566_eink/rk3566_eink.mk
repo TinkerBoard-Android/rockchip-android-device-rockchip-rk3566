cd8e4c0efef6ee18945bb2a558e5201e23a18bba
Dayao Ji <jdy@rock-chips.com>
2021-01-08 09:59:37 +0800

    rk3566_eink: enable recovery eink_fb
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>
    Change-Id: I21899864e8c9c8489813edcb177f0172abe22a8e

--- rk3566_eink/rk3566_eink.mk
+++ rk3566_eink/rk3566_eink.mk
@@ -56,4 +56,5 @@ PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320 \
                               sys.eink.mode=7 \
                               sys.eink.rgba2y4_by_rga=1 \
                               persist.sys.idle-wakeup=false \
-                              persist.sys.idle-delay=5000
+                              persist.sys.idle-delay=5000 \
+                              sys.eink.recovery.eink_fb = true
