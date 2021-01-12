c1621deaa2b62b353389eb5203d07134d9b2a325
wh <wanghang@rock-chips.com>
2021-01-12 17:26:30 +0800

    rk3566_eink: add ro.vendor.eink=true for eink product
    Signed-off-by: wh <wanghang@rock-chips.com>
    Change-Id: I375adef8333adf90a5087b104ab0ae8ec8e55210

--- rk3566_eink/rk3566_eink.mk
+++ rk3566_eink/rk3566_eink.mk
@@ -54,5 +54,6 @@ PRODUCT_PACKAGES += \
 ## add Rockchip properties
 #
 PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320 \
+                              ro.vendor.eink=true \
                               sys.eink.mode=7 \
                               sys.eink.rgba2y4_by_rga=1
