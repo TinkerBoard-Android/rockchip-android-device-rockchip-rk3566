ac805a7939b0c135ed700810d0761be9431393e5
Bian Jin chen <kenjc.bian@rock-chips.com>
2021-01-27 10:33:10 +0800

    device: disable partial_updates for RK356x.
    Test: com.android.server.cts.GraphicsStatsValidationTest
    
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I50845818fae37b927b48476e9a6be37d0608f727

--- device.mk
+++ device.mk
@@ -5,6 +5,10 @@ PRODUCT_PACKAGES += \
     displayd \
     libion
 
+# Disable partial updates
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.use_partial_updates=false
+
 BOARD_SEPOLICY_DIRS += vendor/rockchip/hardware/interfaces/neuralnetworks/1.0/default/sepolicy
 PRODUCT_PACKAGES += \
     librknnhal_bridge \
