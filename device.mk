f6089aeb800427dd16b1367971e4ef31c9c5534e
Wu Liangqing <wlq@rock-chips.com>
2021-02-02 20:06:20 +0800

    set ro.build.shutdown_timeout=6
    fix shutdown dumpinfo:
    [   74.118959] lsof: COMMAND     PID       USER   FD      TYPE
    DEVICE  SIZE/OFF       NODE NAME
    [   74.118959]
    [   74.119038] lsof: init          1       root  cwd       DIR
    253,0      4096          2 /
    [   74.119038]
    [   74.119052] lsof: init          1       root  rtd       DIR
    253,0      4096          2 /
    [   74.119052]
    [   74.119065] lsof: init          1       root  txt       REG
    253,0   1028888       1482 /syst
    em/bin/init
    [   74.119065]
    [   74.119080] lsof: init          1       root  mem       REG
    253,0   1028888       1482 /syst
    em/bin/init
    [   74.119080]
    [   74.119103] lsof: init          1       root  mem       REG
    0,18    131072      18539 /dev/
    __properties__/properties_serial
    [   74.119103]
    [   74.119121] lsof: init          1       root  mem       REG
    0,18    131072      18538 /dev/
    __properties__/u:object_r:wifi_prop:s0
    [   74.119121]
    [   74.119139] lsof: init          1       root  mem       REG
    0,18    131072      18537 /dev/
    __properties__/u:object_r:wifi_log_prop:s0
    [   74.119139]
    [   74.119155] lsof: init          1       root  mem       REG
    0,18    131072      18536 /dev/
    __properties__/u:object_r:vold_prop:s0
    [   74.119155]
    [   74.119172] lsof: init          1       root  mem       REG
    0,18    131072      18535 /dev/
    __properties__/u:object_r:vndk_prop:s0
    [   74.119172]
    [   74.121624] sysrq: Show backtrace of all active CPUs
    [   74.121724] sysrq: Show Blocked State
    [   74.121739]   task                        PC stack   pid father
    [   74.121860] kconsole        D    0    45      2 0x00000028
    [   74.121878] Call trace:
    [   74.121916]  __switch_to+0x130/0x13c
    [   74.121951]  0xffffffc07f6d5cc0
    [   74.122165] sysrq: Kill All Tasks
    [   74.131359] ueventd: 5 output lines suppressed due to ratelimiting
    
    Change-Id: I03b8b14cc0ebc4d4f0214babeb398bf09d92e57e

--- device.mk
+++ device.mk
@@ -87,5 +87,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
                 wifi.supplicant_scan_interval=15 \
                 ro.factory.tool=0 \
                 ro.kernel.android.checkjni=0 \
-                ro.build.shutdown_timeout=0 \
+                ro.build.shutdown_timeout=6 \
                 persist.enable_task_snapshots=false
